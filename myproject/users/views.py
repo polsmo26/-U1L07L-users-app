fc4d58c74d942387980cc08b5dd9b99e51cc9617
Polina <polsmo26@bergen.org>
2024-10-23 09:37:14 -0400

    10/23/24

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -10,6 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             return redirect("posts.list")
+    form = UserCreationForm()
     context = {
         'active_link' : 'register',
         'form':form
